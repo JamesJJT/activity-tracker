68f236fa6df880051270c8ef0043e2aafcff7beb
JamesJJT <jastevens2222@hotmail.co.uk>
2025-02-26 18:41:44 +0000

    fix: Test user creation

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
 
         User::create([
             'name' => 'Test User',
-            'email' => 'test@test.com',
+            'email' => 'test@user.com',
             'password' => Hash::make('password'),
         ]);
 
